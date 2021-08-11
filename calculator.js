c9abd49f9396bf341751e22a4fc813f72d7ad1e5
hjhjhj00 <dlapdlffldpald1@naver.com>
2021-08-12 06:27:40 +0900

    handler 함수 안에서 if문을 통해 전역변수 firstnum 값을 update할 수 있다. 3클릭 계속 해보기.

--- calculator.js
+++ calculator.js
@@ -2,13 +2,40 @@ const numbers= Array.from(document.querySelectorAll(".number"));
 const operations=Array.from(document.querySelectorAll(".operation"));
 console.log(operations, Array.isArray(operations));
 
+/*
+attachNum(a){
 
+}
+//numberClick의 num변수를 return 시키는 대신 attchNum function. => no. numberClick() handler function 안에서 구현해 보자.
+*/
+
+var firstnum = 0// 이면 if(firstnum) 은 거짓이다!
+var firstvalue = 0
+
+/*
+function u(){
+ if(firstnum){
+     console.log("a");
+ }else if(2){ //일단 숫자 있는 경우 true로 친단 걸 확인.
+     console.log("b");
+ }
+}
+u();
+*/
 
 function numberClick(event){
     //console.log(typeof(parseInt(event.target.innerText)));//event.target.value 하면 안되고 innerText하면 된다.
-    const num=parseInt(event.target.innerText)//data type: number
+    const num=event.target.innerText//data type: string
+    //attachNum(num);
+    //return num
+    if(!firstnum){//처음에 firstnum이 0이므로 이걸 실행시키려면 !firstnum해야 함.
+        firstnum=num
+        console.log(firstnum);
+    }else{
+        console.log("firstnum:"+firstnum);
+    }
+    
 
-    return num
     
 };
 
