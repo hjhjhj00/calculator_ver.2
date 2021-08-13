c61d6962f1cb18ee9a52e0a75c19a0a254b4df0c
hjhjhj00 <dlapdlffldpald1@naver.com>
2021-08-13 15:12:19 +0900

    include bracket method. 아직 backspace는 구현x

--- calculator.js
+++ calculator.js
@@ -16,12 +16,63 @@ var conversion;
 var sign;
 var bracket;
 var arr=[];
-
+var open;
+var close;
+var arrSlice;
 //console.log(parseInt("2")+"+"+parseFloat("3.5")+"*"+parseInt("10"));
 /*
 var arr=[2,"+",3.5,"X",54,"÷",9,"X",2,"-",6,"÷",3];
-var arr=["(",1,"+",2,")","X",5,"÷",5.5]
+var arr=["(",1,"+",2,")","X",5,"÷",5.5,"+","(",5,"-",1,")"]
 console.log(arr);
+*/
+function calculate(arr){
+    for(var i=0; i<arr.length; i++){
+        if(arr[i]=="X" || arr[i]=="÷"){
+            if(arr[i]=="X"){
+                arr[i]=arr[i-1]*arr[i+1]
+                arr.splice(i-1,3,arr[i])
+                console.log("This is :"+arr[3])
+                i=i-2
+            }else if(arr[i]=="÷"){
+                arr[i]=arr[i-1]/arr[i+1]
+                arr.splice(i-1,3,arr[i])
+                i=i-2
+            }
+        console.log("length: "+arr.length);
+        }
+    }
+    for(var i=0; i<arr.length; i++){
+        if(arr[i]=="+" || arr[i]=="-"){
+            if(arr[i]=="+"){
+                console.log("init"+arr+"i :"+i)
+                arr[i]=arr[i-1]+arr[i+1]
+                arr.splice(i-1,3,arr[i])
+                
+                console.log("last :"+arr+"i :"+i)
+                i=i-2//이대로 ㄱㅊ??
+            }else if(arr[i]=="-"){
+                arr[i]=arr[i-1]-arr[i+1]
+                arr.splice(i-1,3,arr[i])
+                i=i-2
+            }
+        console.log("length: "+arr.length);
+        }
+    }
+}
+/*
+while(arr.indexOf("(")!=-1){
+    open=arr.indexOf("(")
+    close=arr.indexOf(")")
+    console.log(close-open+1)
+    arrSlice=arr.slice(open,close+1)
+    arrSlice.shift()
+    arrSlice.pop()
+    console.log("arrSlice :"+arrSlice)
+    calculate(arrSlice);
+    console.log("result :"+arrSlice);
+    arr.splice(open,close-open+1,arrSlice[0])
+    console.log(arr)
+}
 
 for(var i=0; i<arr.length; i++){
     if(arr[i]=="X" || arr[i]=="÷"){
@@ -56,9 +107,9 @@ for(var i=0; i<arr.length; i++){
     }
 }
 console.log(arr)
-*/
-//var length=arr.length
 
+//var length=arr.length
+*/
 function numberClick(event){
     const num=event.target.innerText//data: string
         if(!firstnum){
@@ -80,7 +131,9 @@ function numberClick(event){
 };
 
 function operationClick(event){
-    arr.push(firstvalue);
+    if(arr[arr.length-1]!=")"){
+        arr.push(firstvalue)
+    }
     console.log(arr);
     operation=event.target.innerText
     console.log(operation)
@@ -128,66 +181,41 @@ var close;
 var arrSlice;
 //var bracketResult;
 function equalClick(){
-    arr.push(firstvalue)
+    console.log(arr[arr.length-1]!=")")
+    console.log(arr[arr.length-1])
+    if(arr[arr.length-1]!=")"){
+        arr.push(firstvalue)
+    }
+    
     console.log(arr);
-    /*
-    console.log(arr.indexOf("("),arr.indexOf(")"));
 while(arr.indexOf("(")!=-1){
     open=arr.indexOf("(")
     close=arr.indexOf(")")
     console.log(close-open+1)
     arrSlice=arr.slice(open,close+1)
-    for(var i=0; i<arrSlice.length; i++){
-        if(arrSlice[i]=="X" || arrSlice[i]=="÷"){
-            if(arrSlice[i]=="X"){
-                arrSlice[i]=arrSlice[i-1]*arrSlice[i+1]
-                arrSlice.splice(i-1,3,arrSlice[i])
-                console.log("This is :"+arrSlice[3])
-                i=i-2
-            }else if(arrSlice[i]=="/"){
-                arrSlice[i]=arrSlice[i-1]/arrSlice[i+1]
-                arrSlice.splice(i-1,3,arrSlice[i])
-                i=i-2
-            }
-        console.log("length: "+arrSlice.length);
-        }
-    }
-    for(var i=0; i<arrSlice.length; i++){
-        if(arrSlice[i]=="+" || arrSlice[i]=="-"){
-            if(arrSlice[i]=="+"){
-                console.log("init"+arrSlice+"i :"+i)
-                arrSlice[i]=arrSlice[i-1]+arrSlice[i+1]
-                arrSlice.splice(i-1,3,arrSlice[i])
-                
-                console.log("last :"+arrSlice+"i :"+i)
-                i=i-2//이대로 ㄱㅊ??
-            }else if(arrSlice[i]=="-"){
-                arrSlice[i]=arrSlice[i-1]-arrSlice[i+1]
-                arrSlice.splice(i-1,3,arrSlice[i])
-                i=i-2
-            }
-        console.log("length: "+arrSlice.length);
-        }
-    }
     arrSlice.shift()
     arrSlice.pop()
+    console.log("REALarrSlice :"+arrSlice)
+    calculate(arrSlice);
+    console.log("REALresult :"+arrSlice);
     console.log("arrSlice"+arrSlice+arrSlice.length)
-    
     arr.splice(open,close-open+1,arrSlice[0])
-}*/
+    console.log("REAL :"+arr)
+}
+console.log("Initial:"+arr)
+calculate(arr);
+/*
 for(var i=0; i<arr.length; i++){
     if(arr[i]=="X" || arr[i]=="÷"){
         if(arr[i]=="X"){
             arr[i]=arr[i-1]*arr[i+1]
             arr.splice(i-1,3,arr[i])
-            console.log("This is :"+arr[3])
             i=i-2
         }else if(arr[i]=="/"){
             arr[i]=arr[i-1]/arr[i+1]
             arr.splice(i-1,3,arr[i])
             i=i-2
         }
-    console.log("length: "+arr.length);
     }
 }
 for(var i=0; i<arr.length; i++){
@@ -196,131 +224,20 @@ for(var i=0; i<arr.length; i++){
             console.log("init"+arr+"i :"+i)
             arr[i]=arr[i-1]+arr[i+1]
             arr.splice(i-1,3,arr[i])
-            
-            console.log("last :"+arr+"i :"+i)
             i=i-2//이대로 ㄱㅊ??
         }else if(arr[i]=="-"){
             arr[i]=arr[i-1]-arr[i+1]
             arr.splice(i-1,3,arr[i])
             i=i-2
         }
-    console.log("length: "+arr.length);
     }
 }
+*/
 console.log(arr)
 arr=[]
 firstnum=0
 }
 
-
-
-
-/*
-function calculator(a,b){
-    switch(a){
-        case("+"):
-            return value=value+b;
-        case("-"):
-            return value=value-b;
-        case("X"):
-            return value=value*b;
-        case("÷"):
-            return value=value/b;
-    }
-}
-
-
-
-function numberClick(event){
-    const num=event.target.innerText//data: string
-        if(!firstnum){
-            firstnum=num
-            console.log("num: "+firstnum);
-        }else if(firstnum && point){
-            firstnum=firstnum+point+num
-            console.log("point in firstnum:"+firstnum);
-            point=null;
-        }else if(firstnum && !point){
-            firstnum=firstnum+num
-            console.log("firstnum:"+firstnum);
-        }
-        firstvalue = parseFloat(firstnum)
-        console.log(firstvalue);
-    
-};
-
-
-function operationClick(event){
-    mainCalculating();//중요!!operation 주기 전에 value를 초기화시킴.
-    operation=event.target.innerText
-    console.log(operation)
-    firstnum=0
-}
-
-function mainCalculating(){
-    if(!operation){
-        value=firstvalue;
-        console.log("No operation");
-    }else{
-        calculator(operation,firstvalue);
-        
-        console.log("operation exist?: "+operation);
-        console.log(value);
-        operation=null; //안해도 자동으로 초기화(??)되는 듯.
-
-    }
-}
-
-
-function conversionClick(event){
-    conversion=event.target.innerText
-    if(conversion == "1/x"){
-        firstvalue=1/firstvalue}else if(conversion=="x²"){
-        firstvalue=Math.pow(firstvalue,2)}else if(conversion == "√x"){
-        firstvalue=Math.sqrt(firstvalue)}
-    console.log(firstvalue);
-    /*
-    switch(conversion){
-        case("1/x"):
-            return firstvalue=1/firstvalue;
-        case("x²"):
-            return firstvalue=Math.pow(firstvalue,2);
-        case("√x"):
-            return firstvalue=Math.sqrt(firstvalue);
-    }
-    console.log(firstvalue);//왜 이건 안찍힐까?? if절에서는 찍히고 console.log에서는 안찍힘..! 아마 switch 다음부터는 다 무시(?)되는 거 같은데..! why???
-    
-    conversion=null;//없어도 되긴 할듯? 그냥 남아있는게 찝찝해서..
-    
-}
-
-function signClick(event){
-    sign=(-1)
-    firstvalue=firstvalue*sign;
-    console.log(firstvalue);
-    sign=null;
-}
-
-function bracketClick(event){
-    bracket=event.target.innerText;
-    console.log(bracket);
-}
-
-
-function equalClick(){
-    mainCalculating();
-    console.log(value);
-    firstvalue=value;//중요!! result value값을 firstvalue로 넣어 바로 operation 누르고 연산이 가능함.
-    firstnum=0//number click event가 시작되면 firstvalue를 초기화해주는 역할(아마도)
-}
-
-function pointClick(){
-    console.log("There's original operation: "+operation)
-    point="."
-    console.log(point, point === ".")
-}
-
-*/
 
 numbers.forEach(function(number){
     number.addEventListener("click",numberClick);
